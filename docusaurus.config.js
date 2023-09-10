87f4b12e2aaec6863aeccd042bbe53b37cab19dc
hkawa90 <hideo.90.kawamura@gmail.com>
2023-09-10 18:47:18 +0900

    Modify site info

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,15 +4,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 (module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  title: 'Dotsubo',
+  tagline: '主にJavascript/C/C++プログラミング',
+  url: 'https://hkawa90.github.io',
+  baseUrl: '/Dotsubo/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'hkawa90', // Usually your GitHub org/user name.
+  projectName: 'Dotsubo', // Usually your repo name.
 
   presets: [
     [
